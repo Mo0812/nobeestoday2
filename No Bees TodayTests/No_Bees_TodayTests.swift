cc25163a72ea26d45f78d6668c3210f90cd75a16
mkanzler <developer@moritzkanzler.de>
2018-11-08 12:48:28 +0100

    add unit tests

---
+++ No Bees TodayTests/No_Bees_TodayTests.swift
@@ -0,0 +1,34 @@
+//
+//  No_Bees_TodayTests.swift
+//  No Bees TodayTests
+//
+//  Created by Moritz Kanzler on 08.11.18.
+//  Copyright © 2018 Moritz Kanzler. All rights reserved.
+//
+
+import XCTest
+
+class No_Bees_TodayTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
